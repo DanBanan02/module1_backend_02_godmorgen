32d269c0fcaaf10f439e442caf6da5fb382737d4
DanBanan02 <dan.pg26.kodehode@gmail.com>
2026-06-08 11:23:57 +0200

    foreach fix it

--- Program.cs
+++ Program.cs
@@ -1,23 +1,14 @@
 ﻿
 Console.Clear();
-// Char Extraction / use of char.
-// .Length er antallet, begynner fra 0 til 15.
-// samme som while loop, men må ha track i stede. (condition)
-string MyText = "Hello, Person";
 
-Console.WriteLine(MyText.Length);
+string myText = "hello, World.";
 
-Console.WriteLine(MyText[0]);
-Console.Write(MyText[1]);
-Console.Write(MyText[2]);
-
-Console.WriteLine(MyText[15]);
-for (int i = 0; i < MyText.Length; i++)
+for (int i = 0; i < myText.Length; i++)
 {
-    Console.Write(MyText[i]);
+    /* Console.Write(MyText[i]); */
 }
 
-foreach (var item in collection)
+foreach (var Noob in myText)
 {
-    
+    Console.Write(Noob);
 }
