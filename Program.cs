77f93f3ffba24309d004f0e7f0a6f1e0ff3a8412
DanBanan02 <dan.pg26.kodehode@gmail.com>
2026-06-09 10:15:01 +0200

    Static Void Blue print

--- Program.cs
+++ Program.cs
@@ -1,53 +1,28 @@
 ﻿// clear the console
 Console.Clear();
 
-// Array = Type collection
+// Methods (also called as "fuctions")
 
-// Arrays, are fixed length and type (but we can replace existing item).
+// Show Greeting in Console
+// Declarasion of a methods
 
-string[] products = {"Coffe", "Tea", "Bun"};
+// Add a string to the static void
 
-// Replace insted.
-products[0] = "Juice";
-
-
-Console.WriteLine("Our Products");
-// Products er connected with {Items inside brackets.}
-Console.WriteLine(products);
-
-// int, bool, char, string, Objects, arrays, ...
-
-// Arrays, are fixed lenght and types.
-
-foreach (var product in products)
+static void Greet(string customGreeting)
 {
-    Console.WriteLine(product);
+    Console.WriteLine("Hello!");
 }
 
-Console.WriteLine();
+// Navn etter fulgt av ();
+/* Greet();
+ */
 
-// Adress the product by number:
-/* Console.WriteLine(products[0]); */
+// Function calld by X amount of loops
+ for (int i = 0; i < 5; i++)
+ {
+    // if string add "" or add text inside "Text"
+    Greet("Text" + i + "nth time!");
+ }
 
-
-
-Console.ReadLine(); // Temporary break.
-// list: are basically a collection that can grow in size.
-
-// a shopping ( Basket/Cart ) as a list ( Example: )
-
-List<string> cart = new List<string>();
-
-cart.Add("Coffe");
-cart.Add("Soup");
-
-Console.WriteLine("Your Shopping cart" + cart.Count);
-
-foreach (string item in cart)
-{
-    Console.WriteLine(item);
-}
-
-// Removes a spesific item in cart.
-cart.Remove("Soup");
-
+// Consume the method
+Console.Write("");
