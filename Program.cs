ece956d729296b7a8f095008509258693c2732b9
DanBanan02 <dan.pg26.kodehode@gmail.com>
2026-06-10 09:59:28 +0200

    module1_backend_02_godmorgen: TimeCheck v1.4 ( Fixed Lines )

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ class TimeMenu
     public void ShowResult(string text)
     {
         Console.WriteLine();
-
         Console.ForegroundColor = ConsoleColor.DarkGreen;
         Console.WriteLine("=============================");
         Console.ResetColor();
@@ -15,7 +14,6 @@ class TimeMenu
         Console.ForegroundColor = ConsoleColor.DarkGreen;
         Console.WriteLine("=============================");
         Console.ResetColor();
-
         Console.WriteLine();
     }
 
@@ -178,226 +176,23 @@ class MenuList
 
         }
 
-        Console.WriteLine("Exiting TimeCheck...");
-        Console.WriteLine("");
-        Console.WriteLine(" -- Localizing folder: System32");    
-        Console.WriteLine("");
+            Console.WriteLine("Exiting TimeCheck...");
+            Console.WriteLine("");
+            Console.WriteLine(" -- Localizing folder: System32");    
+            Console.WriteLine("");
 
-        // Waste of Space...
-        // Made before foreach fix update...
+            Console.Write("Loading: 0%");
 
-  /*    Console.WriteLine("1%");
-        await Task.Delay(500);;
-        Console.WriteLine("2%");
-        await Task.Delay(500);
-        Console.WriteLine("3%");
-        await Task.Delay(500);
-        Console.WriteLine("4%");
-        await Task.Delay(500);
-        Console.WriteLine("5%");
-        await Task.Delay(500);
-        Console.WriteLine("6%");
-        await Task.Delay(500);
-        Console.WriteLine("7%");
-        await Task.Delay(500);
-        Console.WriteLine("8%");
-        await Task.Delay(500);
-        Console.WriteLine("9%");
-        await Task.Delay(500);
-        Console.WriteLine("10%");
-        await Task.Delay(500);
-        Console.WriteLine("11%");
-        await Task.Delay(500);
-        Console.WriteLine("12%");
-        await Task.Delay(500);
-        Console.WriteLine("13%");
-        await Task.Delay(500);
-        Console.WriteLine("14%");
-        await Task.Delay(500);
-        Console.WriteLine("15%");
-        await Task.Delay(500);
-        Console.WriteLine("16%");
-        await Task.Delay(500);
-        Console.WriteLine("17%");
-        await Task.Delay(500);
-        Console.WriteLine("18%");
-        await Task.Delay(500);
-        Console.WriteLine("19%");
-        await Task.Delay(500);
-        Console.WriteLine("20%");
-        await Task.Delay(500);
-        Console.WriteLine("21%");
-        await Task.Delay(500);
-        Console.WriteLine("22%");
-        await Task.Delay(500);
-        Console.WriteLine("23%");
-        await Task.Delay(500);
-        Console.WriteLine("24%");
-        await Task.Delay(500);
-        Console.WriteLine("25%");
-        await Task.Delay(500);
-        Console.WriteLine("26%");
-        await Task.Delay(500);
-        Console.WriteLine("27%");
-        await Task.Delay(500);
-        Console.WriteLine("28%");
-        await Task.Delay(500);
-        Console.WriteLine("29%");
-        await Task.Delay(500);
-        Console.WriteLine("30%");
-        await Task.Delay(500);
-        Console.WriteLine("31%");
-        await Task.Delay(500);
-        Console.WriteLine("32%");
-        await Task.Delay(500);
-        Console.WriteLine("33%");
-        await Task.Delay(500);
-        Console.WriteLine("34%");
-        await Task.Delay(500);
-        Console.WriteLine("35%");
-        await Task.Delay(500);
-        Console.WriteLine("36%");
-        await Task.Delay(500);
-        Console.WriteLine("37%");
-        await Task.Delay(500);
-        Console.WriteLine("38%");
-        await Task.Delay(500);
-        Console.WriteLine("39%");
-        await Task.Delay(500);
-        Console.WriteLine("40%");
-        await Task.Delay(500);
-        Console.WriteLine("41%");
-        await Task.Delay(500);
-        Console.WriteLine("42%");
-        await Task.Delay(500);
-        Console.WriteLine("43%");
-        await Task.Delay(500);
-        Console.WriteLine("44%");
-        await Task.Delay(500);
-        Console.WriteLine("45%");
-        await Task.Delay(500);
-        Console.WriteLine("46%");
-        await Task.Delay(500);
-        Console.WriteLine("47%");
-        await Task.Delay(500);
-        Console.WriteLine("48%");
-        await Task.Delay(500);
-        Console.WriteLine("49%");
-        await Task.Delay(500);
-        Console.WriteLine("50%");
-        await Task.Delay(500);
-        Console.WriteLine("51%");
-        await Task.Delay(500);
-        Console.WriteLine("52%");
-        await Task.Delay(500);
-        Console.WriteLine("53%");
-        await Task.Delay(500);
-        Console.WriteLine("54%");
-        await Task.Delay(500);
-        Console.WriteLine("55%");
-        await Task.Delay(500);
-        Console.WriteLine("56%");
-        await Task.Delay(500);
-        Console.WriteLine("57%");
-        await Task.Delay(500);
-        Console.WriteLine("58%");
-        await Task.Delay(500);
-        Console.WriteLine("59%");
-        await Task.Delay(500);
-        Console.WriteLine("60%");
-        await Task.Delay(500);
-        Console.WriteLine("61%");
-        await Task.Delay(500);
-        Console.WriteLine("62%");
-        await Task.Delay(500);
-        Console.WriteLine("63%");
-        await Task.Delay(500);
-        Console.WriteLine("64%");
-        await Task.Delay(500);
-        Console.WriteLine("65%");
-        await Task.Delay(500);
-        Console.WriteLine("66%");
-        await Task.Delay(500);
-        Console.WriteLine("67%");
-        await Task.Delay(500);
-        Console.WriteLine("68%");
-        await Task.Delay(500);
-        Console.WriteLine("69%");
-        await Task.Delay(500);
-        Console.WriteLine("70%");
-        await Task.Delay(500);
-        Console.WriteLine("71%");
-        await Task.Delay(500);
-        Console.WriteLine("72%");
-        await Task.Delay(500);
-        Console.WriteLine("73%");
-        await Task.Delay(500);
-        Console.WriteLine("74%");
-        await Task.Delay(500);
-        Console.WriteLine("75%");
-        await Task.Delay(500);
-        Console.WriteLine("76%");
-        await Task.Delay(500);
-        Console.WriteLine("77%");
-        await Task.Delay(500);
-        Console.WriteLine("78%");
-        await Task.Delay(500);
-        Console.WriteLine("79%");
-        await Task.Delay(500);
-        Console.WriteLine("80%");
-        await Task.Delay(500);
-        Console.WriteLine("81%");
-        await Task.Delay(500);
-        Console.WriteLine("82%");
-        await Task.Delay(500);
-        Console.WriteLine("83%");
-        await Task.Delay(500);
-        Console.WriteLine("84%");
-        await Task.Delay(500);
-        Console.WriteLine("85%");
-        await Task.Delay(500);
-        Console.WriteLine("86%");
-        await Task.Delay(500);
-        Console.WriteLine("87%");
-        await Task.Delay(500);
-        Console.WriteLine("88%");
-        await Task.Delay(500);
-        Console.WriteLine("89%");
-        await Task.Delay(500);
-        Console.WriteLine("90%");
-        await Task.Delay(500);
-        Console.WriteLine("91%");
-        await Task.Delay(500);
-        Console.WriteLine("92%");
-        await Task.Delay(500);
-        Console.WriteLine("93%");
-        await Task.Delay(500);
-        Console.WriteLine("94%");
-        await Task.Delay(500);
-        Console.WriteLine("95%");
-        await Task.Delay(500);
-        Console.WriteLine("96%");
-        await Task.Delay(500);
-        Console.WriteLine("97%");
-        await Task.Delay(500);
-        Console.WriteLine("98%");
-        await Task.Delay(500);
-        Console.WriteLine("99%");
-        await Task.Delay(500);
-        Console.WriteLine("100%"); */
+        for (int i = 0; i <= 100; i += 1)
+        {
+            await Task.Delay(150);
 
-    Console.Write("Loading: 0%");
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write($"Loading: {i}%   ");
+        }
 
-for (int i = 0; i <= 100; i += 1)
-{
-    await Task.Delay(150);
-
-    Console.SetCursorPosition(0, Console.CursorTop);
-    Console.Write($"Loading: {i}%   ");
-}
-
-Console.WriteLine();
-Console.WriteLine("Finished!");
+        Console.WriteLine();
+        Console.WriteLine("Finished!");
 
         await Task.Delay(2500);
 
